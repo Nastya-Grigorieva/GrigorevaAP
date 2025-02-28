dd39a672e183680962873fc8f33a4c9721532c51
Anastasiia Grigoreva <grinastich@gmail.com>
2025-02-28 21:11:38 +0500

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
+//test build
 rl.question('Сколько вам лет? ', (age) => {
     console.log(`Вам ${age} лет`);
     rl.close(); 
